3d5c869cf9364310e7522093a4fc9d641ed764c7
Ian Marx <iongonga@kabarak.ac.ke>
2024-02-16 21:44:14 +0300

    Kafka confluent config, serializing the producer and schema registry creation for the client

--- config.py
+++ config.py
@@ -1,4 +1,16 @@
-config = {
+from typing import Dict
+config: Dict[str, str] = {
     "google_api_key": "AIzaSyA-L6R_UEsEmABZLZyU1s9LP4Pq57-P1ho",
     "youtube_playlist_id": "PLFs4vir_WsTwEd-nJgVJCZPNL3HALHHpF",
+    "kafka": {
+        "bootstrap.servers": "pkc-4r087.us-west2.gcp.confluent.cloud:9092",
+        "security.protocol": "SASL_SSL",
+        "sasl.mechanism": "PLAIN",
+        "sasl.username": "4AAZEAGIGYYGETTL",
+        "sasl.password": "SuDBboq0ywDndldigndVaFWeI8mkBPDZGYDx8GbCxm9Ei/C8vjqNcmtb/Qypgobi",
+    },
+    "schema_registry": {
+        "url": "https://psrc-2225o.us-central1.gcp.confluent.cloud",
+        "basic.auth.user.info": "5CXRHRZPFV3WUOFW:QFlBhOOqMjaxyZc+zYYlo+cytxJOCwOrpvIeo0vAYnVW2GVg8IgHxhHhhIzQy4x9",
+    }
 }
